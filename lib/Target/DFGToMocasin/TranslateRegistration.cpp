47d4dc63ad822915f083d1d54e05d705b5b1666e
Robert Khasanov <robert.khasanov@tu-dresden.de>
2026-09-16 20:57:46 +0200

    DFGToMocasin: register dialects used by actor bodies

--- lib/Target/DFGToMocasin/TranslateRegistration.cpp
+++ lib/Target/DFGToMocasin/TranslateRegistration.cpp
@@ -3,6 +3,7 @@
 /// @file
 /// @author     Giuseppe Meloni (giuseppe.meloni@abinsula.com)
 
+#include "mlir/InitAllDialects.h"
 #include "mlir/Tools/mlir-translate/Translation.h"
 #include "laksa-mlir/Dialect/DFG/IR/DFG.h"
 #include "laksa-mlir/Target/DFGToMocasin/MocasinEmitter.h"
@@ -23,7 +24,12 @@ void registerDFGToMocasinTranslation()
         [](Operation* op, raw_ostream &output) {
             return dfg::translateDFGToMocasinYAML(op, output);
         },
-        [](DialectRegistry &registry) { registry.insert<dfg::DFGDialect>(); });
+        [](DialectRegistry &registry) {
+            // Actor bodies retain computation dialects after DFG extraction,
+            // even though the exporter only uses the graph topology.
+            registerAllDialects(registry);
+            registry.insert<dfg::DFGDialect>();
+        });
 }
 
 } // namespace mlir
